370957f54f277ee3c09c02f3fd418c81cd8f192a
MoKa <126101510+Long-Beard90@users.noreply.github.com>
2023-10-28 20:16:41 +0330

    Add files via upload

---
+++ standard deviation.py
@@ -0,0 +1,16 @@
+# importing pandas (test only)
+import pandas as pd
+# given list
+given_list = [1,2,3,4]
+# converting the given_list to DataFrame
+dataList = pd.DataFrame(given_list)
+# calculating the  standard devaition using std() function
+standarddevList = dataList.std()
+# print the given list and standard deviation
+print("The given list of numbers : ")
+for i in given_list:
+    print(i, end=" ")
+# printing new empty line
+print()
+# printing the standard deviation of the given list of numbers
+print("Standard deviation of the given list =", standarddevList)
